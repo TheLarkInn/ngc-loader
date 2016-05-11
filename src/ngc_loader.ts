60a7f54f96d163eb43ce82c5609d8b8a0bcfc3b7
Sean Larkin <Sean.Larkin@mutualofomaha.com>
2016-05-11 16:08:23 -0500

    not quite there

--- src/ngc_loader.ts
+++ src/ngc_loader.ts
@@ -1,24 +1,35 @@
 import 'ts-metadata-collector';
 import 'reflect-metadata';
+import * as ts from 'ts';
 import {lstatSync} from 'fs';
 import {CodeGenerator, MetadataWriterHost, NodeReflectorHost, TsickleHost} from '@angular/compiler-cli';
-import {singleTsc} from './single_tsc';
+import {singleTsc, check} from './single_tsc';
 
 var ngCompiler = require('@angular/compiler-cli/main.js');
 var fs = require('fs');
 var ts = require('typescript');
 var path = require('path');
 
-function ngcLoader(source) {
-  var compiler = this._compiler,
-      compilation = this._compilation;
-  
-  this.cacheable && this.cacheable();
-  
 
-  singleTsc.readConfiguration(compiler.context, compiler.context);
-  
-  return singleTsc.recieveAndEmitSingle(source);
+function ngcLoader(source, sourceMaps) {
+  this.PARSED_SOURCES = ts.TranspileOutput[] = [];
+  try {
+    this.cacheable && this.cacheable();
+    var compiler = this._compiler,
+        compilation = this._compilation;
+    
+    let configRead = singleTsc.readConfiguration(compiler.context, compiler.context);
+    check(configRead.parsed.errors);
+    
+    let parsedSource = singleTsc.recieveAndEmitSingle(source);
+    check(parsedSource.diagnostics) && this.PARSED_SOURCES.push(parsedSource);
+
+    console.log(this.PARSED_SOURCES);
+
+    this.callback(null, parsedSource.outputText, sourceMaps);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default ngcLoader; 
--- src/single_tsc.ts
+++ src/single_tsc.ts
@@ -4,6 +4,33 @@ import {Tsc, check} from '@angular/compiler-cli/tsc';
 import {lstatSync} from 'fs';
 import {AngularCompilerOptions} from '@angular/compiler-cli/codegen';
 
+const DEBUG = true;
+const SOURCE_EXTENSION = /\.[jt]s$/;
+
+function debug(msg: string, ...o: any[]) {
+  if (DEBUG) console.log(msg, ...o);
+}
+
+export function formatDiagnostics(diags: ts.Diagnostic[]): string {
+  return diags.map((d) => {
+                let res = ts.DiagnosticCategory[d.category];
+                if (d.file) {
+                  res += ' at ' + d.file.fileName + ':';
+                  const {line, character} = d.file.getLineAndCharacterOfPosition(d.start);
+                  res += (line + 1) + ':' + (character + 1) + ':';
+                }
+                res += ' ' + ts.flattenDiagnosticMessageText(d.messageText, '\n');
+                return res;
+              })
+      .join('\n');
+}
+
+export function check(diags: ts.Diagnostic[]) {
+  if (diags && diags.length && diags[0]) {
+    throw new Error(formatDiagnostics(diags));
+  }
+}
+
 export class SingleTsc extends Tsc {
   // Make sure that this.readConfiguration() is performed from outside
   public parsedSingle: ts.TranspileOutput;
@@ -26,7 +53,7 @@ export class SingleTsc extends Tsc {
       {readDirectory: ts.sys.readDirectory}, 
       singleBasePath
     );
-    
+
     check(this.parsed.errors);
     // Default codegen goes to the current directory
     // Parsed options are already converted to absolute paths
@@ -40,7 +67,7 @@ export class SingleTsc extends Tsc {
   	check(this.parsedSingle.diagnostics);
 
     this.ngOptions.genDir = path.join(this.singleBasePath, this.ngOptions.genDir || '.');
-    return this.parsedSingle.outputText;
+    return this.parsedSingle;
   }
 }
 
