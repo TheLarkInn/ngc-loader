95edb6248cd1751f7a9d3df2ce62d4d53ea88c78
Sean Larkin <Sean.Larkin@mutualofomaha.com>
2016-05-11 15:23:21 -0500

    Got it transpiling holy crap I wrote a ts-loader

--- src/ngc_loader.ts
+++ src/ngc_loader.ts
@@ -1,32 +1,24 @@
 import 'ts-metadata-collector';
 import 'reflect-metadata';
 import {lstatSync} from 'fs';
-
-var path = require('path');
-var fs = require('fs');
-var path = require('path');
-var ts = require('typescript');
-
-
-var ngCompiler = require('@angular/compiler-cli/main.js');
 import {CodeGenerator, MetadataWriterHost, NodeReflectorHost, TsickleHost} from '@angular/compiler-cli';
 import {singleTsc} from './single_tsc';
 
+var ngCompiler = require('@angular/compiler-cli/main.js');
+var fs = require('fs');
+var ts = require('typescript');
+var path = require('path');
 
-var ngcLoader = function(source) {
-  var compiler = this._compiler;
-  var compilation = this._compilation;
+function ngcLoader(source) {
+  var compiler = this._compiler,
+      compilation = this._compilation;
   
-  debugger;
   this.cacheable && this.cacheable();
   
-  
+
   singleTsc.readConfiguration(compiler.context, compiler.context);
   
   return singleTsc.recieveAndEmitSingle(source);
 }
-
-
-
 
 export default ngcLoader; 
--- src/single_tsc.ts
+++ src/single_tsc.ts
@@ -21,8 +21,12 @@ export class SingleTsc extends Tsc {
     const {config, error} = ts.readConfigFile(project, ts.sys.readFile);
     check([error]);
 
-    this.parsed =
-        ts.parseJsonConfigFileContent(config, {readDirectory: ts.sys.readDirectory}, singleBasePath);
+    this.parsed = ts.parseJsonConfigFileContent(
+      config, 
+      {readDirectory: ts.sys.readDirectory}, 
+      singleBasePath
+    );
+    
     check(this.parsed.errors);
     // Default codegen goes to the current directory
     // Parsed options are already converted to absolute paths
@@ -32,11 +36,6 @@ export class SingleTsc extends Tsc {
   }
 
   recieveAndEmitSingle(source: string) {
-	// destructure config and error vars and check for errors
-	// const {config, error} = ts.readConfigFile(project, ts.sys.readFile);
-	// check([error]);
-
-	// parse individual
   	this.parsedSingle = ts.transpileModule(source, this.parsed.options); 
   	check(this.parsedSingle.diagnostics);
 
